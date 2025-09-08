59051e5253d442e30808aa7ebd89bf9848c65690
yohannes <jyohannes@gmail.com>
2025-09-08 09:07:42 -0900

    header element

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-
+import SearchIcon from '@mui/icons-material/Search';
+import NetflixLogo from "../../assets/NetflixLogo.png"
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 function Header() {
   return (
-    <div>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
+    <div className='header-outer-container'>
+      <div className='header-left'>
+        <ul>
+          <li><img src={NetflixLogo} alt="" /></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div className='header-right'>
+        <SearchIcon />
+        <NotificationsNoneIcon />
+        <AccountBoxIcon />
+        <ArrowDropDownIcon />
+        
+      </div>
     </div>
   )
 }
