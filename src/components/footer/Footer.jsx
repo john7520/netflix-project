07c812aad8f9708e598e9323cb5bb946545e6d91
yohannes <jyohannes@gmail.com>
2025-09-08 16:32:11 -0900

    header and footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
-
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import "./footer.css"
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer-outer-container'>
+      <div className='footer-data'>
+        <div>
+          <FacebookOutlinedIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+          
+          <ul>
+            <li>Audio Description</li>
+            <li>Invective Relations</li>
+            <li>Legal Notice</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Help Center</li>
+            <li>Jobs</li>
+            <li>Cookies Preferences</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Gift Card</li>
+            <li>Term of use</li>
+            <li>Corporation Information</li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <li>Media center</li>
+            <li>Privacy</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+      </div>
+
+      <div>
+        <ul>
+          <li><button>service code</button></li>
+          <li>&copy; 2025 netflix.inc</li>
+        </ul>
+      </div>
+    </div>
   )
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,18 +4,19 @@ import NetflixLogo from "../../assets/NetflixLogo.png"
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import "./header.css"
 function Header() {
   return (
     <div className='header-outer-container'>
       <div className='header-left'>
         <ul>
-          <li><img src={NetflixLogo} alt="" /></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li><img className='logo' src={NetflixLogo} alt="" width={100}/></li>
+          <li>Home</li>
+          <li>TvShows</li>
+          <li>Movies</li>
+          <li>Latest</li>
+          <li>MyList</li>
+          <li>Browse My Languages</li>
         </ul>
       </div>
       <div className='header-right'>
