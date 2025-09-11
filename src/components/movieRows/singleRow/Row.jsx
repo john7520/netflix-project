e547ecbe7560a03ff77edce5c1a474c34b43abd0
yohannes <jyohannes@gmail.com>
2025-09-10 23:29:53 -0900

    add Movies display

--- src/components/movieRows/singleRow/Row.jsx
+++ src/components/movieRows/singleRow/Row.jsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from 'react'
+import movieTrailer from "movie-trailer"
 import "./row.css"
 import Links from '../../../VideoDataLinks/VideoDataLinks';
 import Axios from '../../../VideoDataLinks/Axois';
+import CloseIcon from '@mui/icons-material/Close';
 function Row(props) {
   const baseUrl = "https://image.tmdb.org/t/p/original";
   const [movie, setMovie] = useState([]);
+  const [open, setOpen] = useState(false)
+  const [videoId, setVideoId] = useState("")
+  
+  
   const { title, movieUrl ,isLarge} = props;
   useEffect(() => {
     
@@ -20,13 +26,33 @@ function Row(props) {
       FetchData()
     
   }, [movieUrl])
-  console.log(movie[0]?.poster_path)
+  const [Trailer, setTrailer] = useState("");
+  function HandleMovie(element) {
+    if (Trailer) {
+      setTrailer("")
+    } else {
+      movieTrailer(element?.name || element?.title)
+        .then((url) => {
+          let watchId = url.split("=")
+          let videoId = watchId[1]
+          if (videoId) {
+            setVideoId(videoId)
+          }
+          
+      })
+      
+    }
+  }
   return (
-    <div>
+    <>
       <h1 className="main-title">{title}</h1>
       <div className="movie-images">
         {movie.map((singleMovie) => (
           <img
+            onClick={() => {
+              setOpen(true)
+              HandleMovie(singleMovie)
+            }}
             key={singleMovie.id}
             className={isLarge ? "large poster-image" : "original poster-image"}
             src={`${baseUrl}${
@@ -36,7 +62,26 @@ function Row(props) {
           />
         ))}
       </div>
-    </div>
+
+      <div  className={`modal  ${open? "active":"hidden"}`} id="videoModal">
+          <div className="modal-content">
+          <span onClick={() => {
+            setVideoId("")
+            setOpen(false)
+          }}
+            className="close">
+              <CloseIcon/>
+          </span>
+          
+          {videoId&&<iframe
+              src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=0`}
+              title="YouTube video player"
+              allow="autoplay; encrypted-media"
+              allowFullScreen
+            ></iframe>}
+          </div>
+        </div>
+    </>
   );
 }
 
